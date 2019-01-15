18d0c03c953620e4c7711ced551aceb694682e4a
sugurusasaki <sugurusasaki@macserver.local>
2019-01-15 09:30:08 +0900

    Initial Commit

---
+++ PureDIExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PureDIExample
+//
+//  Created by sugurusasaki on 2019/01/15.
+//  Copyright © 2019 sugurusasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
